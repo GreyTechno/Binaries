85d6d844edf1ab353b8e0862783db190f46ab4d9
MR_GT <111515426+GreyTechno@users.noreply.github.com>
2025-01-02 11:43:21 -0800

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,13 +1,3 @@
-from flask import Flask, jsonify, request
+import streamlit as st
 
-# Initialize the Flask application
-app = Flask(__name__)
-
-# Define a route for the homepage
-@app.route('/')
-def home():
-    return "Welcome to the Flask App!"
-
-# Run the application
-if __name__ == '__main__':
-    app.run(debug=True)
+st.write("Hello world")
